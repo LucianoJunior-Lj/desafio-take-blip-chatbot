5e74bb8ee8e7efbc9250c2dd089aa65477c4823b
LucianoJunior <lucianojunior4242@gmail.com>
2022-07-18 15:39:57 -0300

    feat: add função para requisição ao dos dados

---
+++ challenge/Api/utils/request.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+const api = axios.create({
+  baseURL: 'https://api.github.com/orgs/takenet',
+});
+
+const requestData = async (endpoint) => {
+  const { data } = await api.get(endpoint);
+  return data;
+}
+
+module.exports = requestData;
